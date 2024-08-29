04b7bd43d53e01a159811a8a267635809a0f9d5d
Ahmed Abdelhafiez <nevoexists@gmail.com>
2024-08-29 10:14:40 +0300

    fixed generate more colors bug

--- src/js/script.js
+++ src/js/script.js
@@ -4,17 +4,6 @@ const toastBox = document.querySelector(".toast-box");
 
 // create color elements
 const noOfColors = 12;
-const generateColorElements = function (numOfColors) {
-  for (let i = 0; i < numOfColors; i++) {
-    const colorEl = document.createElement("div");
-    colorEl.classList.add("color");
-    containerEl.appendChild(colorEl);
-  }
-};
-generateColorElements(noOfColors);
-
-const colorElements = document.querySelectorAll(".color");
-
 const generateColors = function () {
   const chars = "0123456789abcdef";
   let color = "";
@@ -25,11 +14,18 @@ const generateColors = function () {
   }
   return "#" + color;
 };
+const generateColorElements = function (numOfColors) {
+  const html = `<div class="color"></div>`;
+  for (let i = 0; i < numOfColors; i++) {
+    const colorEl = document.createElement("div");
+    colorEl.classList.add("color");
+    containerEl.appendChild(colorEl);
+    colorEl.textContent = colorEl.style.backgroundColor = generateColors();
+  }
+};
+generateColorElements(noOfColors);
 
-colorElements.forEach((color) => {
-  color.textContent = color.style.backgroundColor = generateColors();
-});
-
+// Copy to clipboard
 const copyColor = async function (content) {
   try {
     await navigator.clipboard.writeText(content);
@@ -38,6 +34,7 @@ const copyColor = async function (content) {
   }
 };
 
+// Toast messages
 const renderMessage = function (msg) {
   const toast = document.createElement("div");
   toast.classList.add("toast");
@@ -48,7 +45,7 @@ const renderMessage = function (msg) {
   if (msg.includes("clipboard")) {
     toast.classList.add("done");
   }
-  if (msg.includes("Refreshed")) {
+  if (msg.includes("Generated")) {
     toast.classList.add("refresh");
   }
 
@@ -67,6 +64,7 @@ containerEl.addEventListener("click", function (e) {
   renderMessage(doneMsg);
 });
 
+// window load toast message
 window.addEventListener("load", function (e) {
   toastBox.innerHTML = "";
   const loadMsg = `<i class="fa-solid fa-bell"></i> Click on any color to copy it.`;
@@ -76,6 +74,8 @@ window.addEventListener("load", function (e) {
 // dark mode
 const inputEl = document.querySelector(".input");
 const h1El = document.querySelector("h1");
+
+//get mode status from local storage
 inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
 const updateBackground = function () {
@@ -89,6 +89,7 @@ const updateBackground = function () {
 };
 updateBackground();
 
+// add mode status to local storage
 const updateLocalStorage = function () {
   localStorage.setItem("mode", JSON.stringify(inputEl.checked));
 };
@@ -100,13 +101,8 @@ inputEl.addEventListener("input", function () {
 // refresh btn
 const addMoreBtn = document.querySelector(".btn");
 addMoreBtn.addEventListener("click", function () {
-  containerEl.innerHTML = "";
   generateColorElements(noOfColors);
-  const colorElements = document.querySelectorAll(".color");
-  colorElements.forEach((color) => {
-    color.textContent = color.style.backgroundColor = generateColors();
-  });
   toastBox.innerHTML = "";
-  const loadMsg = `<i class="fa-solid fa-arrows-rotate"></i> Refreshed.`;
+  const loadMsg = `<i class="fa-solid fa-check"></i> Generated Successfully.`;
   renderMessage(loadMsg);
 });
